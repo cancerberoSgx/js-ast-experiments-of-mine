c03114d2b06a9a02b10caf06ab053ebd50f9f7ad
Sebastián Gurin <sebastigurin@gmail.com>
2018-07-03 03:47:08 -0300

    build

--- src/ui/modal/selectedExampleDescriptionModal.tsx
+++ src/ui/modal/selectedExampleDescriptionModal.tsx
@@ -1,30 +1,30 @@
 import React from 'react';
 import { getState } from '../../workspace';
 
-export default ()=>
+export default () =>
 
-<div className={"modal fade"} id="selectedExampleDescriptionModal" role="dialog" aria-labelledby="selectedExampleDescriptionModalLabel" aria-hidden="true">
-  <div className={"modal-dialog modal-lg"} role="document">
-    <div className={"modal-content"}>
-      <div className={"modal-header"}>
-        <h5 className={"modal-title"} id="selectedExampleDescriptionModalLabel">What's this</h5>
-        <button type="button" className={"close"} data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div className={"modal-body"}>
-      
-        <h3>{getState().selectedExample.name}</h3>
+  <div className={"modal fade"} id="selectedExampleDescriptionModal" role="dialog" aria-labelledby="selectedExampleDescriptionModalLabel" aria-hidden="true">
+    <div className={"modal-dialog modal-lg"} role="document">
+      <div className={"modal-content"}>
+        <div className={"modal-header"}>
+          <h5 className={"modal-title"} id="selectedExampleDescriptionModalLabel">{getState().selectedExample.name}</h5>
+          <button type="button" className={"close"} data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div className={"modal-body"}>
 
-        <h4>Description</h4>
+          <p>The current example name is "{getState().selectedExample.name}"</p>
 
-        <p>{getState().selectedExample.description}</p>
+          {getState().selectedExample.description.split('\n').map(line =>
+            <p>{line}</p>
+          )}
 
-      </div>
-      <div className={"modal-footer"}>
-        <button type="button" className={"btn btn-secondary"} data-dismiss="modal">Close</button>
+        </div>
+        <div className={"modal-footer"}>
+          <button type="button" className={"btn btn-secondary"} data-dismiss="modal">Close</button>
+        </div>
       </div>
     </div>
   </div>
-</div>
 
